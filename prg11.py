c67ac8288cbc5978c344adc7527beaf4798168c7
Pradeepa0503 <99667927+Pradeepa0503@users.noreply.github.com>
2022-02-22 20:11:41 +0530

    add 11th file

---
+++ prg11.py
@@ -0,0 +1,5 @@
+#Write a program to search for element in an array and return it's position
+a=[1,2,3,4,5,6,7,8,9,10]
+i=int(input("enter the number to find it's position: "))
+n=a.index(i)
+print(n)
